38665ab9873d1aba426d9df6f4211e1ddb4d197d
Gatien Anizan <gatien.anizan@gmail.com>
2020-11-04 15:52:22 +0100

    add store persistance

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import { auth, post } from './modules';
 
 Vue.use(Vuex);
 
+// Active la persistance du store pour éviter de
+// se relog à chaque rechargement d'un page
+const vuexLocal = new VuexPersistence({
+  // Sotckage du store dans le localStorage
+  // (peut aussi etre stocker en sessionStorage)
+  storage: window.localStorage,
+});
+
 export default new Vuex.Store({
   modules: {
+    // Gestion de l'autentification de l'utilisateur
     auth,
+    // Gestion des posts
     post,
   },
+  plugins: [
+    // Plugin de persistance
+    vuexLocal.plugin,
+  ],
 });
