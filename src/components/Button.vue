6cb06aa7fd6000fc118d20c4c8ab12a36a11eb42
Gatien Anizan <gatien.anizan@gmail.com>
2020-11-06 16:26:41 +0100

    Button & File Input component

---
+++ src/components/Button.vue
@@ -0,0 +1,64 @@
+<template>
+  <button class="v-button" @click="$emit('click')">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.v-button {
+  all: unset;
+  display: flex;
+  align-items: center;
+  border: 1px black solid;
+  padding: 0 15px;
+  height: 30px;
+  cursor: pointer;
+  background: white;
+  font-weight: 500;
+  transition: all .2s ease;
+
+  &.big {
+    padding: 0 25px;
+    height: 40px;
+    font-size: 18px;
+  }
+
+  &.center {
+    justify-content: center;
+  }
+
+  &.underline {
+    border: 0;
+    text-decoration: underline;
+    padding: 0;
+  }
+
+  &:disabled {
+    border: 1px gray solid;
+    color: gray;
+    pointer-events: none;
+  }
+
+  &:hover {
+    background: black;
+    color: white;
+
+    svg {
+      fill: white;
+    }
+  }
+
+  svg {
+    fill: black;
+    height: 60%;
+    padding-left: 10px;
+    transition: all .2s ease;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'customButton',
+};
+</script>
---
+++ src/components/FileInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <label for="file">
+    Choose a file to upload
+    <upload-icon/>
+    <input
+      type="file"
+      name="file"
+      id="file"
+      @change="$emit('change', $event)"
+      :accept="accept"/>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+label {
+  all: unset;
+  display: flex;
+  align-items: center;
+  border: 1px black solid;
+  padding: 0 25px;
+  height: 40px;
+  cursor: pointer;
+  background: white;
+  font-weight: 500;
+  font-size: 18px;
+  transition: all .2s ease;
+
+  .upload-icon {
+    height: 60%;
+    padding-left: 10px;
+    transition: all .2s ease;
+  }
+
+  input {
+    width: 0.1px;
+    height: 0.1px;
+    opacity: 0;
+    overflow: hidden;
+    position: absolute;
+    z-index: -1;
+  }
+
+  &:hover {
+    background: black;
+    color: white;
+
+    .upload-icon {
+      fill: white;
+    }
+  }
+}
+</style>
+
+<script>
+import UploadIcon from '@/components/UploadIcon.vue';
+
+export default {
+  name: 'FileInput',
+  components: {
+    'upload-icon': UploadIcon,
+  },
+  props: {
+    accept: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
